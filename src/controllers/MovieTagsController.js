fc9259708f2dec77e8f384010f95a9649ff56673
ruiamcosta <ram666.rc@gmail.com>
2024-07-09 09:47:32 +0100

    add movie tags migration, routes and controller

---
+++ src/controllers/MovieTagsController.js
@@ -0,0 +1,13 @@
+const knex = require('../database/knex')
+
+class MovieTagsController {
+  async index(req, res) {
+    const { user_id } = req.params
+
+    const movie_tags = await knex('movie_notes').where({ user_id })
+
+    return res.json(movie_tags)
+  }
+}
+
+module.exports = MovieTagsController
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const Router = require('express')
 const userRoutes = require('./usersRoutes')
 const movieNotesRoutes = require('./movieNotesRoutes')
+const movieTagsRoutes = require('./movieTagsRoutes')
 
 const routes = Router()
 
 routes.use('/users', userRoutes)
 routes.use('/movie-notes', movieNotesRoutes)
+routes.use('/movie-tags', movieTagsRoutes)
 
 module.exports = routes
