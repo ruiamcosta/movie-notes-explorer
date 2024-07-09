a45523ed134ce0ed636dcce266f3ca6deca6d528
ruiamcosta <ram666.rc@gmail.com>
2024-07-09 10:11:25 +0100

    add insert tags on create method of MovieNotesController

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -5,12 +5,22 @@ class MovieNotesController {
     const { title, description, rating, tags } = req.body
     const { user_id } = req.params
 
-    await knex('movie_notes').insert({
+    const [note_id] = await knex('movie_notes').insert({
       title,
       description,
       rating,
       user_id
     })
+
+    const tagsInsert = tags.map(name => {
+      return {
+        name,
+        note_id,
+        user_id
+      }
+    })
+
+    await knex('movie_tags').insert(tagsInsert)
 
     return res.status(201).json()
   }
