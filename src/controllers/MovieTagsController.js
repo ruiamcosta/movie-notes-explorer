4f129fdf6a4539365edaa1462e2cb0c52c35cac6
ruiamcosta <ram666.rc@gmail.com>
2024-07-09 15:12:55 +0100

    fix index method of MovieTagsController

--- src/controllers/MovieTagsController.js
+++ src/controllers/MovieTagsController.js
@@ -4,7 +4,7 @@ class MovieTagsController {
   async index(req, res) {
     const { user_id } = req.params
 
-    const movie_tags = await knex('movie_notes').where({ user_id })
+    const movie_tags = await knex('movie_tags').where({ user_id })
 
     return res.json(movie_tags)
   }
